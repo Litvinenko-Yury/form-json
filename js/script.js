2e93984bcc1910ec53d57e59dc0397e891386443
Литвиненко Юрий <litvinenko-don@ukr.net>
2020-12-27 19:17:54 +0300

    feat: добавил функцию вывода на страницу Selected plan: #...

--- js/script.js
+++ js/script.js
@@ -1,44 +1,48 @@
 'use strict';
 
-const radioWrapper = document.querySelector('#radio'),
-  labelInput = radioWrapper.querySelectorAll('.field-radio__input'),
-  select = document.querySelector('#select');
 
-/*функция, считает TOTAL: sum*/
-function calcTotal() {
-  labelInput.forEach(item => {
-    if (item.checked) {
-      let sum = item.value * select.value;
-      setTotalSum(sum);
+document.addEventListener('DOMContentLoaded', () => {
+  const radioWrapper = document.querySelector('#radio'),
+    labelInput = radioWrapper.querySelectorAll('.field-radio__input'),
+    select = document.querySelector('#select');
+
+  calcTotal(); // старт функции после загрузки страницы
+
+  /*функция, считает TOTAL: sum*/
+  function calcTotal() {
+    labelInput.forEach((item, i) => {
+      if (item.checked) {
+        let sum = item.value * select.value,
+          num = i + 1;
+
+        setTotalSum(sum);
+        setInfoText((num));
+      }
+    });
+  }
+
+  /*функция, устанавливает на страницу значение TOTAL: sum*/
+  function setTotalSum(a) {
+    document.querySelector('#sum').textContent = `$${a}`;
+  }
+
+  /*функция, устанавливает на страницу значение Selected plan: #*/
+  function setInfoText(a) {
+    document.querySelector('#num').textContent = `${a}`;
+  }
+
+  /*=====================*/
+  //обработчики
+  /*=====================*/
+  radioWrapper.addEventListener('click', (event) => {
+    if (event.target.classList.contains('field-radio__input')) {
+      calcTotal();
     }
   });
-}
 
-/*функция, устанавливает на страницу значение TOTAL: sum*/
-function setTotalSum(a) {
-  document.querySelector('#sum').textContent = `$${a}`;
-}
-
-/*функция, устанавливает на страницу значение Selected plan: #*/
-function setPlanText(a) {
-
-}
-
-
-/*обработчик на все radioWrapper*/
-radioWrapper.addEventListener('click', (event) => {
-  if (event.target.classList.contains('field-radio__input')) {
+  select.addEventListener('click', () => {
     calcTotal();
-  }
-});
-
-/*обработчик на select*/
-select.addEventListener('click', () => {
-  calcTotal();
+  });
 });
 
 
-
-
-
-/**/
